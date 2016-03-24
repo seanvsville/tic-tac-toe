296b6d1efc5a06dbcf5ae591033bd1788622593f
seanvsville <seanvanho@gmail.com>
2016-03-23 19:31:04 -0700

    fixed board to reset at end of game, added controls for theme song, renamed title and header

--- js/app.js
+++ js/app.js
@@ -4,9 +4,18 @@ $(document).ready(function() {
   // All code to manipulate the DOM
   // Goes inside this function
 
-// Setting turn = 0, Player icon, and Player
+// Setting turn = 0
 
 var turn = 0;
+
+// Set function to reset board
+
+  function resetBoard(event) {
+    $(".box").text("");
+    $(".box").removeClass(".box");
+    //reset turn
+    turn = 0;
+  }
 
 // Creating checkWin function via checking against assigned box IDs and icon value
 
@@ -14,34 +23,43 @@ var turn = 0;
     if ( $('#box1').html()===icon && $('#box2').html()===icon && $('#box3').html()===icon ) {
       $('#box1, #box2, #box3');
       alert(player + ' wins!!!');
+      resetBoard();
     } else if ( $('#box4').html()===icon && $('#box5').html()===icon && $('#box6').html()===icon ) {
       $('#box4, #box5, #box6');
       alert(player + ' wins!!!');
+      resetBoard();
     } else if ( $('#box7').html()===icon && $('#box8').html()===icon && $('#box9').html()===icon ) {
       $('#box7, #box8, #box9');
       alert(player + ' wins!!!');
+      resetBoard();
     } else if ( $('#box1').html()===icon && $('#box4').html()===icon && $('#box7').html()===icon ) {
       $('#box1, #box4, #box7');
       alert(player + ' wins!!!');
+      resetBoard();
     } else if ( $('#box2').html()===icon && $('#box5').html()===icon && $('#box8').html()===icon ) {
       $('#box2, #box5, #box8');
       alert(player + ' wins!!!');
+      resetBoard();
     } else if ( $('#box3').html()===icon && $('#box6').html()===icon && $('#box9').html()===icon ) {
       $('#box3, #box6, #box9');
       alert(player + ' wins!!!');
+      resetBoard();
     } else if ( $('#box3').html()===icon && $('#box5').html()===icon && $('#box7').html()===icon ) {
       $('#box3, #box5, #box7');
       alert(player + ' wins!!!');
+      resetBoard();
     } else if ( $('#box1').html()===icon && $('#box5').html()===icon && $('#box9').html()===icon ) {
       $('#box1, #box5, #box9');
       alert(player + ' wins!!!');
+      resetBoard();
 
 // Set if condition for draws
 
     } else if (turn === 9) {
       alert("It's a draw!");
+      resetBoard();
     }
-  };
+  }
 
 // Set function for handling turns
 // If whatever box gets selected, apply text to selected box
@@ -67,13 +85,9 @@ var turn = 0;
     }
   });
 
-// handleTurn functions end here //
-
-// Set function to reset board
-
-  $(".btn").click(function resetBoard(event) {
-    $(".box").empty();
-    turn = 0;
+// Event listener to reset board
+$('#reset').on('click', function() {
+    resetBoard();
   });
 
 // End of document functions //
